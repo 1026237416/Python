6b5f8e8378d4f21ed6c33c93ca80c77b135e7a63
1026237416 <1026237416@qq.com>
2018-03-10 00:27:17 +0800

    framework start

--- my_framework/index.py
+++ my_framework/index.py
@@ -14,11 +14,52 @@
 from wsgiref.simple_server import make_server
 
 
+def index():
+    return "Welcome to index! "
+
+
+def about_me():
+    return "Welcome to my website, I am learn python web fromework."
+
+
+def news():
+    return "今日头条："
+
+
+def not_found():
+    return "404 Not Found！"
+
+
+url_list = {
+    "index": index,
+    "about": about_me,
+    "news": news,
+    "default": not_found
+}
+
+
 def run_server(environ, start_response):
     # 1、根据不同的URL返回不同的结果，获取URL
     start_response("200 OK", [("Content-type", "text/html")])
 
-    return "<h1>Hello world!</h1>"
+    request_url = environ["PATH_INFO"]
+    print request_url
+
+    response = ""
+
+    if (request_url == "/") or (request_url == "/index"):
+        response = url_list.get("index")
+    elif start_response == "/about":
+        response = url_list.get("about")
+    elif request_url == "/news":
+        response = url_list.get("news")
+    else:
+        response = url_list.get("default")
+
+    response_data = "<h1>%s</h1>" % response()
+    print response_data
+
+    return response_data
 
 
 if __name__ == '__main__':
